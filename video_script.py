6e4192001cf037e8ba5018d21d31f8fd462d712c
pankaj <kamanipankaj9099@gmail.com>
2018-04-01 11:20:53 +0530

    now streaming in fullScreen

--- video_script.py
+++ video_script.py
@@ -66,23 +66,3 @@ if __name__ == '__main__':
             time.sleep(10)
         x = fetch_data('http://date.jsontest.com')
         print "Schedular date ==> {} time ==> {}".format(x.date, x.time)
-
-# import numpy as np
-# import cv2
-
-# cap = cv2.VideoCapture('1.mp4')
-# while(cap.isOpened()):
-#     ret, frame = cap.read()
-#     if ret == True:
-#         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#         cv2.imshow('frame', frame)
-#         # & 0xFF is required for a 64-bit system
-#         if cv2.waitKey(30) & 0xFF == ord('q'):
-#             break
-#     else:
-#         break
-# cap.release()
-# cv2.destroyAllWindows()
-
-# # if __name__ == '__main__':
-# #     coil(video_queue)
